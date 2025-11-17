bb24b4e9dedb0fca22401ad5d02be78c2fc9b11c
nexoraio99 <nexoraio99@gmail.com>
2025-11-17 16:14:49 +0530

    Update about.js

--- script/about.js
+++ script/about.js
@@ -41,3 +41,21 @@ try {
   document.querySelectorAll('.fade-in-left, .fade-in-right, .fade-in-up, .divider')
     .forEach((el) => { el.classList.add('revealed'); });
 }
+
+/* ==========================================================================
+       Bing UET (unchanged, lazy-load safe)
+       ========================================================================== */
+    function initBingUET() {
+      (function(w, d, t, r, u) {
+        var f, n, i;
+        w[u] = w[u] || [], f = function() {
+          var o = { ti: "343210550", enableAutoSpaTracking: true };
+          o.q = w[u], w[u] = new UET(o), w[u].push("pageLoad");
+        },
+        n = d.createElement(t), n.src = r, n.async = 1, n.onload = n.onreadystatechange = function() {
+          var s = this.readyState;
+          s && s !== "loaded" && s !== "complete" || (f(), n.onload = n.onreadystatechange = null);
+        },
+        i = d.getElementsByTagName(t)[0], i.parentNode.insertBefore(n, i);
+      })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+    }
